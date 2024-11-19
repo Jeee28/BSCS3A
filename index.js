7242f597f19adaafc957db687a25d2cfab184158
Anje Grande <117792546+Jeee28@users.noreply.github.com>
2024-11-19 09:55:49 +0800

    index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(logger)
 app.use(cors())
 //connection to mysql
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "employee",
+    host: "byhborf8b6guxj9zzbqe-mysql.services.clever-cloud.com",
+    user: "us55nsoywowrup03",
+    password: "vUi0dFTxNYYizhJIu0oA",
+    database: "byhborf8b6guxj9zzbqe",
 });
 
 //initilization of connection
